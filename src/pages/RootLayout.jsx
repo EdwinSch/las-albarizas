105cd87d7cf590cf4d9fbc57843a45ad2f030eae
Edwin <edwinschutjes@gmail.com>
2024-07-26 17:43:01 +0200

    add: sidenav test

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,12 +1,25 @@
 import { Outlet } from "react-router-dom";
+import { IoMenu } from "react-icons/io5";
+import { useState } from "react";
 
 const RootLayout = () => {
+  const [toggleNav, setToggleNav] = useState(false);
+
   return (
-    <>
-      <header>Header</header>
+    <main>
+      <nav className={toggleNav ? "nav-wrapper nav-open" : "nav-wrapper"}>
+        nav menu
+      </nav>
+      <button
+        onClick={() => setToggleNav(!toggleNav)}
+        className="nav-btn"
+        type="button"
+      >
+        <IoMenu />
+      </button>
       <Outlet />
       <footer>Footer</footer>
-    </>
+    </main>
   );
 };
 export default RootLayout;
